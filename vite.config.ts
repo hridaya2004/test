956fdea66b7337123f25d98c420fc1f7bd031e99
Hridaya Prajapati <hridaya.prajapati@samey.ai>
2025-07-04 14:24:35 +0545

    some updates
    Change-Id: Ib45c164dd65ecef831aeff4ade64e603794539d3

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import tanstackRouter from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist",
+  },
+  base: "/test/",
   plugins: [
     tanstackRouter({ autoCodeSplitting: true, target: "react" }),
     viteReact(),
