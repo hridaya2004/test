4f4656e4bf82a9e039e312aab925df2029fe5869
Hridaya Prajapati <hridaya.prajapati@samey.ai>
2025-07-04 14:27:48 +0545

    Updates
    Change-Id: Ie50fe7e98cb201e360d4746b27c469223e1b2432

---
+++ assets/_slug-5Nc_A3jL.js
@@ -0,0 +1,1 @@
+import{u as t,j as n}from"./index-BzRlQ4Qs.js";const r=function(){const{slug:o}=t({from:"/chat/$slug"});return n.jsxs("div",{children:['Hello "/chat/"! ',o]})};export{r as component};
---
+++ assets/index-CIXREGGC.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BzRlQ4Qs.js";const e=function(){return o.jsx("div",{children:'Hello "/chat/"!'})};export{e as component};
